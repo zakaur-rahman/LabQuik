0d294b89b88d13c6e9b036d261e482d0a32e8888
Zakaur <zakaurrahman67@gmail.com>
2024-12-11 19:36:06 +0530

    sample name change

--- app/components/patient/update-report/utils/reportActions.ts
+++ app/components/patient/update-report/utils/reportActions.ts
@@ -1,6 +1,6 @@
 import { pdf, Document, Page } from '@react-pdf/renderer';
 import React from 'react';
-import TestReportPDF from '../Sample';
+import TestReportPDF from '../TestReportPDF';
 
 interface ReportOptions {
   showHeaderFooter?: boolean;
